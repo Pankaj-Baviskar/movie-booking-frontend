ee06529a3a016d64459e72be8156c15a253ced78
Pankaj-Baviskar <baviskarpankaj143@gmail.com>
2024-06-29 23:05:47 +0530

    Initial commit with frontend code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
+      <Routes>
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
-      </Switch>
+      </Routes>
     </Router>
   );
 };
