041f2014af1bd379029ec84263d2eb00f32e44e6
Pankaj-Baviskar <baviskarpankaj143@gmail.com>
2024-06-29 11:19:00 +0530

    Updates

---
+++ precache-manifest.b1a1147cd2fdf7efcb8c3f89b67751cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47be1d5973a9b92af1b7e49a6616d64",
+    "url": "/movie-booking-frontend/index.html"
+  },
+  {
+    "revision": "4b36a27bbf988dc1a705",
+    "url": "/movie-booking-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bf0d42061b7003a4a233",
+    "url": "/movie-booking-frontend/static/js/2.8680bc2d.chunk.js"
+  },
+  {
+    "revision": "9d0cc3cfc905af4a6940",
+    "url": "/movie-booking-frontend/static/js/3.69ba5426.chunk.js"
+  },
+  {
+    "revision": "4b36a27bbf988dc1a705",
+    "url": "/movie-booking-frontend/static/js/main.e38c89a2.chunk.js"
+  },
+  {
+    "revision": "4680a08fcb66280cafc1",
+    "url": "/movie-booking-frontend/static/js/runtime~main.04db6dd4.js"
+  }
+]);
