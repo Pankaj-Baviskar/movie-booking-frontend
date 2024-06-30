185bea8c5630b5049c8ad89d1bd9fc72b35ff394
Pankaj-Baviskar <baviskarpankaj143@gmail.com>
2024-06-30 12:05:52 +0530

    Update API URLs to point to the live backend

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,7 +10,7 @@ const Login = () => {
   const onSubmit = async e => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', formData);
+      const res = await axios.post('https://movie-booking-backend.onrender.com/api/auth/login', formData);
       console.log(res.data);
     } catch (err) {
       console.error(err.response.data);
